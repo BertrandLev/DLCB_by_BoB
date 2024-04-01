fd338ed6d8a1110e6ca0026fbf13c82354a3eaac
Meuhmeu <108556148+Meuhmeu@users.noreply.github.com>
2024-04-01 23:32:27 +0200

    modification of GPC class

--- app/utils/GPC_data.py
+++ app/utils/GPC_data.py
@@ -1,10 +1,35 @@
 # GPC_data.py
+import pandas as pd
+import numpy as np
+
 
 # File to import .xls file and treat GPC data to use it
 
 class GPC():
     def __init__(self):
-        pass
+        self.logM = None
+        self.w = None
+        self.dict_info = {}
 
-    def import_file(file_path: str):
-        pass
+    def __repr__(self) -> str:
+        return "\n".join(
+            ['{:<13}'.format(key) + ": " + val for key,val in self.dict_info.items()])
+
+    def import_file(self, file_path: str):
+        # Lecture des données
+        df_file = pd.read_excel(file_path, sheet_name='Data')
+        # Récupération des colonnes LogM et Proba
+        df_data = df_file[['LogM conventional ','Weight fraction / LogM ']]
+        df_data = df_data.rename(columns={'LogM conventional ':'LogM','Weight fraction / LogM ':'w'})
+        df_data = df_data.dropna()
+        df_data = df_data[df_data['LogM'] > 2.2]
+        # Interpolation
+        self.logM = np.linspace(np.min(df_data['LogM']),np.max(df_data['LogM']),1000)
+        self.w = np.interp(self.logM, df_data['LogM'].values[::-1], df_data['w'].values[::-1], )
+        # Lecture des informations
+        df_file = pd.read_excel(file_path, sheet_name='Results')
+        df_file = df_file.iloc[:,1:3]
+        df_file.set_index(df_file.columns[0], drop=True, inplace=True)
+        self.dict_info['Project'] = df_file.loc['Project #',df_file.columns[0]]
+        self.dict_info['Sample'] = df_file.loc['Name',df_file.columns[0]]
+        self.dict_info['Description'] = df_file.loc['Description',df_file.columns[0]]
--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,23 @@
 from app.utils.Flory_fit import fit_N_Flory, plot_N_Flory
+from app.utils.GPC_data import GPC
 import numpy as np
 import pandas as pd
 
 # Import du fichier
-file_path = 'tests\GPCIR TOTAL-23-0267.xls'
-df_file = pd.read_excel(file_path, sheet_name='Data')
-df_data = df_file[['LogM conventional ','Weight fraction / LogM ']]
-df_data = df_data.rename(columns={'LogM conventional ':'LogM','Weight fraction / LogM ':'w'})
-df_data = df_data.dropna()
-df_data = df_data[df_data['LogM'] > 2.2]
-df_data['M'] = np.power(10,df_data['LogM'].values)
+# file_path = 'tests\GPCIR TOTAL-23-0259.xls'
+file_path = 'tests\GPCIR TOTAL-23-0263.xls'
+# file_path = 'tests\GPCIR TOTAL-23-0267.xls'
+data = GPC()
+data.import_file(file_path)
 
-# Interpolation des valeurs sur 1000 points
-logM = np.linspace(np.min(df_data['LogM']),np.max(df_data['LogM']),1000)
-w = np.interp(logM, df_data['LogM'].values[::-1], df_data['w'].values[::-1], )
+print(data)
+
 
 # Lancement du fitting et affichage des résultats
-for N in [1,2,3,4,5,6]:
-    print(f"Fitting par {N} Flory")
-    try:
-        params = fit_N_Flory(logM, w, N)
-        plot_N_Flory(logM,w,N,params)
-    except Exception as e:
-        print("Une erreur est survenue lors du fit:",e)
+# for N in [1,2,3,4,5,6]:
+#     print(f"Fitting par {N} Flory")
+#     try:
+#         params = fit_N_Flory(logM, w, N)
+#         plot_N_Flory(logM,w,N,params)
+#     except Exception as e:
+#         print("Une erreur est survenue lors du fit:",e)
