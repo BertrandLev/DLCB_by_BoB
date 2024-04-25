2657cb785e9a452be2e7f2d586c38844f9df13d7
Meuhmeu <108556148+Meuhmeu@users.noreply.github.com>
2024-04-26 00:12:13 +0200

    BobsimuTab : reorganize

--- app/ui/QWidgetBobSimu.py
+++ app/ui/QWidgetBobSimu.py
@@ -13,9 +13,8 @@ from PyQt6.QtWidgets import (QWidget, QGridLayout, QVBoxLayout, QHBoxLayout, QPu
 
 class Bob_chem_param(QGroupBox):
     
-    def __init__(self, log : Log_box) -> None:
+    def __init__(self) -> None:
         super().__init__("Chemical Parameters")
-        self.log = log
         layout = QGridLayout(self)
         self.M0_entry = QLineEdit()
         self.M0_entry.setText("28.0")
@@ -52,6 +51,10 @@ class Bob_chem_param(QGroupBox):
         }
         return parameters     
 
+class Bob_process_param(QGroupBox):
+
+    def __init__(self) -> None:
+        super().__init__("Process Parameters")
 
 class Bob_componant(QGroupBox):
 
@@ -93,20 +96,25 @@ class BobSimuTab(QWidget):
         # Data
 
         # Main Layout
-        main_layout = QHBoxLayout(self)
+        main_layout = QVBoxLayout(self)
         main_splitter = QSplitter()
-        main_splitter.setOrientation(Qt.Orientation.Horizontal)
+        main_splitter.setOrientation(Qt.Orientation.Vertical)
         main_layout.addWidget(main_splitter)
-        # Left Part
-        left_splitter = QSplitter(main_splitter)
-        left_splitter.setOrientation(Qt.Orientation.Vertical)
-        # Bob Simulation box
-        bob_box = QGroupBox(title="Bob simulation imputs", parent=left_splitter)
-        bob_layout = QGridLayout(bob_box)
-        # Log box
-        self.log = Log_box(title="Simulation Log", parent=left_splitter)
-        self.bob_chem_param = Bob_chem_param(self.log)
-        # Componants box
+        # Top Part
+        top_splitter = QSplitter(main_splitter)
+        top_splitter.setOrientation(Qt.Orientation.Horizontal)
+        # Top left part
+        top_left_box = QGroupBox(title="Bob simulation imputs", parent=top_splitter)
+        top_left_layout = QGridLayout(top_left_box)
+        # Bob Parameters box
+        self.bob_proc_param = Bob_process_param()
+        self.bob_chem_param = Bob_chem_param()
+        top_left_layout.addWidget(self.bob_proc_param,0,0,1,3)
+        top_left_layout.addWidget(self.bob_chem_param,1,0,2,3)
+        # Top right part
+        top_right_box = QGroupBox(parent=top_splitter)
+        top_right_layout = QGridLayout(top_right_box)
+        # Componants
         self.bob_comp_list = []
         bob_comp_scrollArea = QScrollArea()
         bob_comp_scrollArea.setMinimumHeight(300)
@@ -120,36 +128,48 @@ class BobSimuTab(QWidget):
         self.bob_comp_layout.addWidget(self.bob_comp_Nb_value,0,1,1,2,Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
         bob_comp_scrollArea.setWidget(self.bob_comp_box)
         bob_comp_scrollArea.setWidgetResizable(True)
+        # Add output folder selection
+        output_file_box = QGroupBox("Output File")
+        output_file_layout = QGridLayout(output_file_box)
+        self.output_file_entry = QLineEdit("")
+        self.output_file_button = QPushButton("...")
+        output_file_layout.addWidget(self.output_file_entry,0,0,1,3)
+        output_file_layout.addWidget(self.output_file_button,0,3,1,1)
+        output_file_layout.setColumnStretch(0,1)
         # Add start and reset button
         bob_reset_button = QPushButton("Reset")
         bob_reset_button.clicked.connect(self.reset_bob_param)
         bob_start_button = QPushButton("Start")
         bob_start_button.clicked.connect(self.start_bob_simu)
-        bob_layout.addWidget(self.bob_chem_param,0,0,1,3)
-        bob_layout.addWidget(bob_comp_scrollArea,1,0,1,3)
-        bob_layout.addWidget(bob_reset_button,2,1)
-        bob_layout.addWidget(bob_start_button,2,2)
+        # Layout
+        top_right_layout.addWidget(bob_comp_scrollArea,0,0,1,3)
+        top_right_layout.addWidget(output_file_box,1,0,1,3)
+        top_right_layout.addWidget(bob_reset_button,2,1)
+        top_right_layout.addWidget(bob_start_button,2,2)
         
+        # Bottom part : Log box
+        self.log = Log_box(title="Simulation Log", parent=main_splitter)
+
         # Right Part
-        right_splitter = QSplitter(main_splitter)
-        right_splitter.setOrientation(Qt.Orientation.Vertical)
-        #top
-        r_top_part = QWidget(right_splitter)
-        r_top_layout = QVBoxLayout(r_top_part)
-        self.plot_GPC = Plot_box(xlabel="w (s-1)", ylabel="G' | G''")
-        self.plot_GPC.setLogMode(x=True, y=True)
-        RDA_box = QGroupBox("RDA File")
-        RDA_layout = QGridLayout(RDA_box)
-        self.RDA_entry = QLineEdit("")
-        self.RDA_button = QPushButton("...")
-        RDA_layout.addWidget(self.RDA_entry,0,0,1,3)
-        RDA_layout.addWidget(self.RDA_button,0,3,1,1)
-        RDA_layout.setColumnStretch(0,1)
-        r_top_layout.addWidget(RDA_box)
-        r_top_layout.addWidget(self.plot_GPC)
-        #bot
-        r_bot_part = QWidget(right_splitter)
-        r_bot_layout = QVBoxLayout(r_bot_part)
+        # right_splitter = QSplitter(main_splitter)
+        # right_splitter.setOrientation(Qt.Orientation.Vertical)
+        # #top
+        # r_top_part = QWidget(right_splitter)
+        # r_top_layout = QVBoxLayout(r_top_part)
+        # self.plot_GPC = Plot_box(xlabel="w (s-1)", ylabel="G' | G''")
+        # self.plot_GPC.setLogMode(x=True, y=True)
+        # RDA_box = QGroupBox("RDA File")
+        # RDA_layout = QGridLayout(RDA_box)
+        # self.RDA_entry = QLineEdit("")
+        # self.RDA_button = QPushButton("...")
+        # RDA_layout.addWidget(self.RDA_entry,0,0,1,3)
+        # RDA_layout.addWidget(self.RDA_button,0,3,1,1)
+        # RDA_layout.setColumnStretch(0,1)
+        # r_top_layout.addWidget(RDA_box)
+        # r_top_layout.addWidget(self.plot_GPC)
+        # #bot
+        # r_bot_part = QWidget(right_splitter)
+        # r_bot_layout = QVBoxLayout(r_bot_part)
 
         # End of init
         self.log.appendLogMessage("Start of Bob simulation session...")
